9388fb4258203be0aa0f3047b472f9c06f0fe9e1
Joshua Sakolsky-Madaras <121878943+JSakMad@users.noreply.github.com>
2026-08-26 12:42:41 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list) # mapping the character count to list of anagrams
+
+        for s in strs:
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            hashmap[tuple(count)].append(s)
+
+        return list(hashmap.values())
+
